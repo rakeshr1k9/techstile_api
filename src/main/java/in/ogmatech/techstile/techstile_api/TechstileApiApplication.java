0aaa22d1c850a8f2211c08ea6e897a381c782a1e
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-01-16 10:41:12 +0530

    changes

--- src/main/java/in/ogmatech/techstile/techstile_api/TechstileApiApplication.java
+++ src/main/java/in/ogmatech/techstile/techstile_api/TechstileApiApplication.java
@@ -9,11 +9,4 @@ public class TechstileApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TechstileApiApplication.class, args);
 	}
-	//testing
-<<<<<<< HEAD
-
-	//2nd test from Rakesh
-=======
-	//arun's test
->>>>>>> dff74c3ffb1f37e50e4aa6eb62dd8a776af85d5b
 }
