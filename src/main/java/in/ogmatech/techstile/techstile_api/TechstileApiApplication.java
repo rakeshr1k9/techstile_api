4a9f80ab33dcc2a92883ecd10f34e1d9a7af63dc
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-01-16 10:20:51 +0530

    initial commit

---
+++ src/main/java/in/ogmatech/techstile/techstile_api/TechstileApiApplication.java
@@ -0,0 +1,12 @@
+package in.ogmatech.techstile.techstile_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstileApiApplication.class, args);
+	}
+}
---
+++ src/test/java/in/ogmatech/techstile/techstile_api/TechstileApiApplicationTests.java
@@ -0,0 +1,16 @@
+package in.ogmatech.techstile.techstile_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechstileApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
